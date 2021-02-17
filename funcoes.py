612020acfdcc5ff3a7ba5a0da5b41caf71db4bdb
rodryggodev <rodrygolacort@hotmail.com>
2021-02-16 21:24:03 -0300

    subindo python v1

---
+++ funcoes.py
@@ -0,0 +1,20 @@
+# def é uma function(função)
+# posso chamar uma função mais de uma vez
+
+#def big_mac():
+   # print('tá pronto');
+#big_mac();    
+#big_mac();
+
+# calc
+# só vou criar uma variavel dentro da def ou qualquer function caso eu vá precisar dela futuramente
+def soma(n1, n2):
+    res = n1 +  n2;
+    return res;
+print(soma(10, 4));
+print(soma(3,3));
+
+def multi(num1, num2):
+    return num1 * num2;
+resultado = multi(9, 10);
+print(resultado);
